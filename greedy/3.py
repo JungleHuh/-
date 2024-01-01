4a5de8dcb0c4aa1b2a3e61084b0cdb4aa2db9cb8
홍길동 <fofn0766@gmail.com>
2024-01-01 23:12:13 +0900

    202401010

--- greedy/3.py
+++ greedy/3.py
@@ -6,7 +6,8 @@ if data[0] == '1':
     count0 += 1
 else:
     count1 += 1
-
+# data[i] != data[i+1] -> 연속된 수라면, 상황을 고려하지 않고 넘어감.
+# 내가 이해하지 못했던 포인트
 for i in range(len(data)-1):
     if data[i] != data[i+1]:
         if data[i+1] == '1':
