5463cb34f6d7b3b4b646e033cb51071fa4499987
홍길동 <fofn0766@gmail.com>
2024-01-04 08:01:48 +0900

    문자열/정수 변환 주의

--- greedy/2.py
+++ greedy/2.py
@@ -1,5 +1,10 @@
-#곱하기 혹은 더하기
+data = input()
+result = int(data[0])
 
-import sys
-input = sys.stdin.readline
-
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num <=1 or result <= 1:
+        result += num
+    else:
+        result *= num
+print(result)
