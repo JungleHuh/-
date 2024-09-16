a32d2012b416ad8acdc4550434559426d1231e8c
홍길동 <fofn0766@gmail.com>
2024-09-17 02:44:07 +0900

    7

--- Sorting/quick_sort2.py
+++ Sorting/quick_sort2.py
@@ -1,14 +1,16 @@
-array = [5,7,9,0,4,2,6,2,4,8]
+array = [5, 7, 9, 0, 4, 2, 6, 2, 4, 8]
 
 def quick_sort(array):
     if len(array) <= 1:
         return array
-    pivot = array[0]
-    tail = array[1:]
+    pivot = array[0]  # 피벗은 첫 번째 요소
+    tail = array[1:]  # 피벗을 제외한 나머지 리스트
 
-    left_side =  [x for x in tail if x <= pivot]
-    right_side = [ x for x in tail if x > pivot]
+    # 분할: 피벗을 기준으로 작은 값은 왼쪽, 큰 값은 오른쪽
+    left_side = [x for x in tail if x <= pivot]  # 피벗보다 작거나 같은 값
+    right_side = [x for x in tail if x > pivot]  # 피벗보다 큰 값
 
+    # 재귀적으로 왼쪽, 오른쪽 정렬 후 병합
     return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
 print(quick_sort(array))
