17984200d8cf4c22e2af736b2b6cafc60555e5ae
홍길동 <fofn0766@gmail.com>
2024-09-20 12:24:59 +0900

    3

--- 9-ShortestPath/maxHeap.py
+++ 9-ShortestPath/maxHeap.py
@@ -1,4 +1,4 @@
-# 최소 힙(파이썬은 Min_heap이 기본: 낮은데이터부터 꺼내짐: 오름차순 정렬)
+# 최대힙 -> Value를 음수값으로 넣고, 뺄 때 다시 음수값 붙여주어 최대힙 구현
 import heapq
 
 def heapsort(iterable):
@@ -6,10 +6,10 @@ def heapsort(iterable):
     result = []
 
     for value in iterable:
-        heapq.heappush(h, value)
+        heapq.heappush(h, -value)
     
     for i in range(len(h)):
-        result.append(heapq.heappop(h))
+        result.append(-heapq.heappop(h))
     return result 
 
 result = heapsort([1,3,5,7,9,2,4,6,8,0])
