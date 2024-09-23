3f666090f3c8cff979824ac0169029f61199d101
홍길동 <fofn0766@gmail.com>
2024-09-23 18:10:16 +0900

    1

--- 10-UnionFind/10-1.py
+++ 10-UnionFind/10-1.py
@@ -1,1 +1,24 @@
-.
+# 특정 원솩 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+# 두 원소가 속합 집합 합치기
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0]*(v+1)
+
+for i in range(1, v+1):
+    parent[i] = i
+    
