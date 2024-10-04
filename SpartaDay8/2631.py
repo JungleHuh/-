b0a1ab2dee231875a0d97201200f4ebb0dd345f8
홍길동 <fofn0766@gmail.com>
2024-10-04 15:33:34 +0900

    3

---
+++ SpartaDay8/2631.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+# 입력 처리
+n = int(input())
+lst = []
+for i in range(n):
+    lst.append(int(input()))
+
+
+# dp[i]: i번째 수를 마지막으로 하는 증가하는 부분 수열의 최대 합
+dp = [1]*(n+1)  # 처음에는 각 원소 자체가 최대 합으로 초기화
+
+# 증가하는 부분 수열의 합을 구하는 과정
+for i in range(1, n):
+    for j in range(i):
+        if lst[i] > lst[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# dp 배열에서 가장 큰 값이 답
+print(n- max(dp))
